83bfd85181557e8fad925ce3615f4255c6f90c15
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-01-13 20:58:58 +0200

    Replaced some part of code using Array.from .js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,14 +3,9 @@
 'use strict';
 
 const duplicateValues = (stringValue, amountOfDoubles) => {
-  if (amountOfDoubles <= 0) return [];
-  else {
-    let res = [];
-    for (let i = 0; i < amountOfDoubles; i++) {
-      res[i] = stringValue;
-    }
-    return res;
-  }
+  if (amountOfDoubles <= 0) { return []; }
+
+  return Array.from({ length: amountOfDoubles }, () => stringValue);
 };
 
 module.exports = duplicateValues;
