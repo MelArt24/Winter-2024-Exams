c32b92277505189e41ac4adcbb76a49e8a994b94
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-01-13 22:23:19 +0200

    Added 'use strict' and changed function's name.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,6 +1,7 @@
 // Get one random element from an array
+'use strict';
 
-const SAMPLE = (arr) => {
+const getRandomElement = (arr) => {
   arr = arr[Math.floor(Math.random() * arr.length)];
   return arr;
 };
