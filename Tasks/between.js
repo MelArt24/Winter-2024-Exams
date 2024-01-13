81b663812d1c5b489c63adcf6815d7f23b8fe9e4
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-01-13 19:42:05 +0200

    Deleted one of "magic number".js

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,8 +3,10 @@
 
 const getValueBetween = (string, start, end) => {
   let startIndex = string.indexOf(start);
+
+  const SUBSTRING_IS_NOT_FOUND = -1;
   
-  if (startIndex === -1) return '';
+  if (startIndex === SUBSTRING_IS_NOT_FOUND) return '';
   else {
     let endIndex = startIndex + start.length;
     string = string.substring(endIndex);
@@ -12,7 +14,7 @@ const getValueBetween = (string, start, end) => {
     if (end) {
       startIndex = string.indexOf(end);
       
-      if (startIndex === -1) {
+      if (startIndex === SUBSTRING_IS_NOT_FOUND) {
         return '';
       } else {
         string = string.substring(0, startIndex);
